c29b5fab19e41d35ece2ee371c1fe66776d35e7f
Sjoerd van Galen <sjoerdvangalen93@gmail.com>
2017-09-16 14:22:39 +0200

    Chapter 7: ifStatements

---
+++ IfStatement.py
@@ -0,0 +1,21 @@
+x = 2
+y = 7
+z = 10
+
+if x > y:
+    print(x, 'is greater than', y)
+
+if x < y:
+    print(x, 'is less than', y)
+
+if x == y:
+    print(x, 'is the same as', y)
+
+if x == 2:
+    print(x, 'equals the string 2')
+
+if x <= y:
+    print(x, 'is less than or equals the string to', y)
+
+if z > y > x:
+    print(z, 'is greater than', y, 'which is greater than', x)
