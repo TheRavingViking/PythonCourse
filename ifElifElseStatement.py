89859ef039bfd729ef899bdb0e1f15304c74cdec
sjoerdvangalen93@gmail.com <Airwave93>
2017-09-18 16:08:22 +0200

    chapter 9: ifElifElse statements

---
+++ ifElifElseStatement.py
@@ -0,0 +1,12 @@
+x = 3
+y = 7
+z = 10
+
+if x < y:
+    print(x, 'is greater than', y)
+elif x > z:
+    print(x, 'is less than', z)
+elif y < z:
+    print(y, 'is greater than', z)
+else:
+    print('nothing was the case')
